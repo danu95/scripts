cad76c1a2d48e0c17e3558faa3496a86f1580afe
vonkaenel dani <vonkaeneldanigit@proton.me>
2025-11-08 23:07:25 +0100

    -

--- wget_curl_install.sh
+++ wget_curl_install.sh
@@ -49,13 +49,10 @@ echo -e 'Types: deb\nURIs: https://download.vscodium.com/debs\nSuites: vscodium\
 
 sudo apt update && sudo apt install codium -y
 
-
 # Draw.io
 wget -O drawio-amd64-28.1.2.deb "https://release-assets.githubusercontent.com/github-production-release-asset/92443980/f231e53e-56d4-419e-a04f-ff0b366eb5e8?sp=r&sv=2018-11-09&sr=b&spr=https&se=2025-10-05T15%3A27%3A17Z&rscd=attachment%3B+filename%3Ddrawio-amd64-28.1.2.deb&rsct=application%2Foctet-stream&skoid=96c2d410-5711-43a1-aedd-ab1947aa7ab0&sktid=398a6654-997b-47e9-b12b-9515b896b4de&skt=2025-10-05T14%3A26%3A22Z&ske=2025-10-05T15%3A27%3A17Z&sks=b&skv=2018-11-09&sig=7xH2ElEzxfgh%2FgH9A%2Fag5k6oDEFqn6pR5BJ%2BdAwAUU0%3D&jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmVsZWFzZS1hc3NldHMuZ2l0aHVidXNlcmNvbnRlbnQuY29tIiwia2V5Ijoia2V5MSIsImV4cCI6MTc1OTY3Njk2NiwibmJmIjoxNzU5Njc1MTY2LCJwYXRoIjoicmVsZWFzZWFzc2V0cHJvZHVjdGlvbi5ibG9iLmNvcmUud2luZG93cy5uZXQifQ.5X3R7o7Alx4vyNv-ATGbOPaBkjRC-uzriXod9gldPQQ&response-content-disposition=attachment%3B%20filename%3Ddrawio-amd64-28.1.2.deb&response-content-type=application%2Foctet-stream"
 
 sudo apt install ./drawio-amd64-28.1.2.deb -y
-
-
 
 
 
