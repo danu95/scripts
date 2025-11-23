493f26b17143d2b02a275c4a8fe6331109f6c499
vonkaenel dani <vonkaeneldanigit@proton.me>
2025-11-23 22:52:58 +0100

    -

--- kickstart_modular/lua/custom/plugins/init.lua
+++ kickstart_modular/lua/custom/plugins/init.lua
@@ -8,11 +8,29 @@ return {
 	-- vimtex
 	{
 		"lervag/vimtex",
-		lazy = false, -- we don't want to lazy load VimTeX
+		lazy = false, -- load on startup - we don't want ot lazy load VimtTeX
 		-- tag = "v2.15", -- uncomment to pin to a specific release
 		init = function()
-			-- VimTeX configuration goes here, e.g.
+			-- VimTeX configuration goes here:
+			-- PLEASE don't jsut copy it in without reading the comments! :)
+			-- This is necessary for VimTeX to lead properly. The "indent" is optional.
+			-- Note: Most plugin managers will do this automatically!
+			vim.cmd("filetype plugin indent on")
+			-- This enables Vim's and neovim's syntax-related features. Without this, some
+			-- VimTeX features will not work (see ":help vimtex-requirements" for more info).
+			-- Note: Most plugin managers will do this automatically!
+			vim.cmd("syntax enable")
+
+			-- Viewer options: One may configure the viewer either by specifying a built-in viewer method:
 			vim.g.vimtex_view_method = "zathura"
+			-- Or with a generic interface:
+			-- vim.g.vimtex_view_general_viewer = "okular"
+			-- vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
+
+			-- VimTeX uses latexmk as the default compiler backend. If you use it, which is strongly recommended, you probably don't need to configure anything. If you want another compiler backend, you can change it as follows. The list of supported backends and further explanation is provided in the documentation, see ":help vimtex-compiler".
+			-- vim.g.vimtex_compiler_method = "latexrun"
+			-- Most VimTeX mappings rely on localleader and this can be changed with the following line. The default is usually fine and is the symbol "\".
+			-- vim.g.maplocalleader = ","
 		end,
 	},
 
